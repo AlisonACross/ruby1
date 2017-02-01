0be5b1fb1bca8b3fa3917584f35905b323612b30
AlisonACross <alisonacross@gmail.com>
2017-01-31 18:32:11 -0600

    first.rb

---
+++ first.rb
@@ -0,0 +1,9 @@
+print "Do you like jokes? (enter 1 for yes, 0 for no): "
+answer = Integer(gets)
+if (answer == 1)
+    print "Great! How many programmers does it take to change a light bulb? None, thats a hardware problem!"
+elsif (answer == 0)
+    print "Too bad. Whats the object-oriented way to become wealthy? Inheritance."
+else
+    print "You dont know how to follow directions, do you?"
+end
